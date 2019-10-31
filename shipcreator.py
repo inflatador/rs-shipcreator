d37963302b16d297fcd038e15dfd31de7b34a04e
Brian King <1854985+inflatador@users.noreply.github.com>
2019-10-31 18:08:32 -0500

    alpha stage, but working

--- shipcreator.py
+++ shipcreator.py
@@ -1,43 +1,44 @@
 #!/usr/bin/env python3
-# # -*- coding: UTF-8 -*-
-# shipcreator. Given a Rackspace Cloud Network and IP, makes a Shared IP on that network.
-#Note: Shared IP is in limited availabilty and must be manually enabled on your account!
-# version - 0.0.2a
-# © 2018 Brian King,
-##Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# shipcreator.py:
+# given at least 2 cloud server IDs and a network,
+# provisions shared IP on that network
 
-import argparse
-import base64
-import logging
-import plac
+from getpass import getpass
 import json
 import keyring
+import logging
 import os
+import plac
 import requests
-import time
 import sys
+import time
 
-requests.packages.urllib3.disable_warnings()
+def find_endpoints(auth_token, headers, region, desired_service="cloudServersOpenStack"):
+
+    url = ("https://identity.api.rackspacecloud.com/v2.0/tokens/%s/endpoints" % auth_token)
+    #region is always uppercase in the service catalog
+    region = region.upper()
+    raw_service_catalog = requests.get(url, headers=headers)
+    raw_service_catalog.raise_for_status()
+    the_service_catalog = raw_service_catalog.json()
+    endpoints = the_service_catalog["endpoints"]
+
+    for service in endpoints:
+        if desired_service == service["name"] and region == service["region"]:
+            desired_endpoint = service["publicURL"]
+
+    return desired_endpoint
 
 def getset_keyring_credentials(username=None, password=None):
-    """Method to retrieve credentials from keyring."""
+    #Method to retrieve credentials from keyring.
+    print (sys.version_info.major)
     username = keyring.get_password("raxcloud", "username")
     if username is None:
         if sys.version_info.major < 3:
             username = raw_input("Enter Rackspace Username: ")
             keyring.set_password("raxcloud", 'username', username)
             print ("Username value saved in keychain as raxcloud username.")
-        elif creds == "username":
+        elif sys.version_info.major >= 3:
             username = input("Enter Rackspace Username: ")
             keyring.set_password("raxcloud", 'username', username)
             print ("Username value saved in keychain as raxcloud username.")
@@ -49,17 +50,6 @@ def getset_keyring_credentials(username=None, password=None):
         keyring.set_password("raxcloud", 'password' , password)
         print ("API key value saved in keychain as raxcloud password.")
     return username, password
-
-def wipe_keyring_credentials(username, password):
-    """Wipe credentials from keyring."""
-    try:
-        keyring.delete_password('raxcloud', 'username')
-        keyring.delete_password('raxcloud', 'password')
-    except:
-        pass
-
-    return True
-
 # Request to authenticate using password
 def get_auth_token(username,password):
     #setting up api call
@@ -92,73 +82,130 @@ def get_auth_token(username,password):
     data = r.json()
     #assign token and account variables with info from json response.
     auth_token = data["access"]["token"]["id"]
-    tenant_id = data["access"]["token"]["tenant"]["id"]
-    return auth_token, tenant_id
 
-def find_endpoints(auth_token, region, desired_service="cloudNetworks"):
+    headers = ({'content-type': 'application/json', 'Accept': 'application/json',
+    'X-Auth-Token': auth_token})
 
-    url = ("https://identity.api.rackspacecloud.com/v2.0/tokens/%s/endpoints" % auth_token)
-    headers = {'content-type': 'application/json', 'Accept': 'application/json',
-               'X-Auth-Token': auth_token}
-    #region is always uppercase in the service catalog
-    region =region.upper()
-    raw_service_catalog = requests.get(url, headers=headers)
-    raw_service_catalog.raise_for_status()
-    the_service_catalog = raw_service_catalog.json()
-    endpoints = the_service_catalog["endpoints"]
-    for service in range(len(endpoints)):
-        if desired_service == endpoints[service]["name"] and region == endpoints[service]["region"]:
-            desired_endpoint = endpoints[service]["publicURL"]
-    return desired_endpoint, headers
+    return auth_token, headers
 
-def create_neutron_ports(auth_token, headers, neutron_endpoint, net_id):
-    ports_uri = "ports"
-    networks_url = ("%s/%s" % (neutron_endpoint, ports_uri))
-    #before we can make a shared IP, we have to make neutron ports.
-    neutron_port_ids = []
-    for port in '1', '2':
-    	portname = ("port%s" % (port))
-    	payload = {'port': {'name': portname, 'network_id': net_id} }
-    	raw_port_create_req = requests.post(url=networks_url, headers=headers, json=payload)
-    	port_create_req = raw_port_create_req.json()
-    	neutron_port_ids.append(port_create_req["port"]["id"])
-    return neutron_port_ids
-    
-def create_shared_ip(auth_token, headers, neutron_endpoint, net_id, shared_ip, neutron_port_ids):
- 	ips_uri = "ip_addresses"
- 	ips_url = ("%s/%s" % (neutron_endpoint, ips_uri))
- 	print (neutron_port_ids)
- 	payload = ({'ip_address': { 'network_id': net_id,
- 				'version': '4',
- 				'ip_address': shared_ip,
-				'port_ids': neutron_port_ids}
+def validate_cs_ids(cloud_network, cs_endpoint, cs_ids, headers):
+    #some validation for Cloud Server IDs
+    if "," in cs_ids:
+        cs_list = []
+        cs_list = cs_ids.split(',')
+        cs_details = []
+        provisioning_zones = []
+        for cs_id in cs_list:
+            cs_url = "{}/servers/{}".format(cs_endpoint, cs_id)
+            cs_check = requests.get(url=cs_url, headers=headers)
+            #validate that the server exists
+            if cs_check.status_code != 200:
+                print(f"Could not validate server {cs_id}! Cloud Servers "
+                      f"API returns code {cs_check.status_code}. Exiting!")
+                exit(1)
+            cs_details.append(cs_check.json())
+        for cs_detail in cs_details:
+            if cloud_network == "public":
+                print("Server {} is in provisioning zone {}".format(cs_detail["server"]["name"], cs_detail["server"]["RAX-PUBLIC-IP-ZONE-ID:publicIPZoneId"]))
+                provisioning_zones.append(cs_detail["server"]["RAX-PUBLIC-IP-ZONE-ID:publicIPZoneId"])
+        # validate that servers are in the same provisioning zone
+        if len(set(provisioning_zones)) != 1 and cloud_network == "public":
+            print (f"Error! Shared Public IP requires servers to be in the same"
+                  f" provisioning zone!")
+            exit(1)
+        return cs_details, cs_list
+    else:
+        print ("Could not validate input. Exiting!")
+        sys.exit()
+
+def get_port_ids(cloud_network, cn_endpoint, cs_list, headers):
+    # verify the Cloud Network exists, and is currently connected
+    # to the servers
+
+    shared_ip_ports = []
+    for cs in cs_list:
+        # querystuff = {'device_id': cs}
+        if cloud_network == "public":
+            querystuff = { 'network_id': '00000000-0000-0000-0000-000000000000',
+                          'device_id': cs }
+        else:
+            querystuff = {'network_id': cloud_network,
+                          'device_id': cs }
+        ports_url = "{}/ports".format(cn_endpoint)
+        print("Checking if server {} has a port on network {}...".format(cs, cloud_network))
+        port_check = requests.get(url=ports_url, params=querystuff, headers=headers)
+        port_check.raise_for_status()
+        port_list = port_check.json()["ports"]
+        for port in port_list:
+            if cs == port["device_id"]:
+                print ("Found port {} on network {} attached to server {}!".format(port["id"], cloud_network, cs))
+            shared_ip_ports.append(port["id"])
+        # print (shared_ip_ports)
+        print ("Finished loop, and ports are at {}".format(shared_ip_ports))
+    if len(cs_list) != len(shared_ip_ports):
+        print ("Error! At least one Cloud Server has no port on network {}".format(cloud_network))
+        sys.exit(1)
+    return shared_ip_ports
+
+def create_shared_ip(cloud_network, cn_endpoint, headers, ip_version,
+                    shared_ip_ports):
+    if cloud_network == "public":
+        net_id = "00000000-0000-0000-0000-000000000000"
+    else:
+        net_id = cloud_network
+    ips_url = "{}/ip_addresses".format(cn_endpoint)
+    payload = ({'ip_address': { 'network_id': net_id,
+ 				'version': '6',
+				'port_ids': shared_ip_ports}
  				})
- 	raw_ship_create_req = requests.post(url=ips_url, headers=headers, json=payload)
- 	ship_create_req = raw_ship_create_req.json()
- 	print (ship_create_req)
- 	
+    print ("Provisioning Shared IP on network {}".format(cloud_network))
+    ship_create_req = requests.post(url=ips_url, headers=headers, json=payload)
+    print (ship_create_req.json())
+
+
+        # if not ports:
+        #     print ("Error! Could not find a port on network {} attached to server {}!".format(port["id"], cloud_network, cs))
+
+
+
+        #validate that the Cloud Network exists
+        # if port_check.status_code != 200:
+        #     print(f"Could not validate server {cs_id}! Cloud Servers "
+        #           f"API returns code {cs_check.status_code}. Exiting!")
+        #     exit(1)
+
+# def get_port_ids(cloud_network, cn_endpoint, cs_list, headers):
+#     port_ids = []
+#     for cs in cs_list:
+#
+
+
+
 #begin main function
 @plac.annotations(
     region=plac.Annotation("Rackspace Cloud Servers region"),
-    net_id=plac.Annotation("Cloud Network ID"),
-    shared_ip=plac.Annotation("Desired IP to be shared")
+    cs_ids=plac.Annotation("At least 2 Cloud Server IDs, separated by commas"),
+    cloud_network=plac.Annotation("Cloud Network or 'public' for Public Network"),
+    ip_version=plac.Annotation("IP version", choices=["4", "6"])
                 )
-def main(region, net_id, shared_ip):
-    username,password = getset_keyring_credentials()
 
-    auth_token,tenant_id = get_auth_token(username, password)
+def main(region, cs_ids, cloud_network, ip_version):
+    username, password = getset_keyring_credentials()
 
-    find_endpoints(auth_token, region)
+    auth_token, headers = get_auth_token(username, password)
 
-    desired_endpoint, headers = find_endpoints(auth_token, region)
+    cn_endpoint = find_endpoints(auth_token, headers, region,
+              desired_service="cloudNetworks")
 
-    #this might violate DRY, I will fix later.
+    cs_endpoint = find_endpoints(auth_token, headers, region,
+              desired_service="cloudServersOpenStack")
 
-    neutron_endpoint = desired_endpoint
-    
-    neutron_port_ids = create_neutron_ports(auth_token, headers, neutron_endpoint, net_id)
-    
-    create_shared_ip(auth_token, headers, neutron_endpoint, net_id, shared_ip, neutron_port_ids)
+    cs_details, cs_list = validate_cs_ids(cloud_network, cs_endpoint, cs_ids, headers)
+
+    shared_ip_ports = get_port_ids(cloud_network, cn_endpoint, cs_list, headers)
+
+    create_shared_ip(cloud_network, cn_endpoint, headers, ip_version,
+                    shared_ip_ports)
 
 if __name__ == '__main__':
     import plac
